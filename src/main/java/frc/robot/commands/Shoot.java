e1cd81cde4767e9b99fc4b9cfc11836aef06e769
Evan <evankempin1@outlook.com>
2024-01-24 16:06:25 -0600

    Added a Shoot Command

---
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -0,0 +1,19 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class Shoot extends Command {
+
+    public Shoot() {
+
+
+    }
+
+    public void StartShooting() {
+
+    }
+
+    public void StopShooting() {
+
+    }
+}
